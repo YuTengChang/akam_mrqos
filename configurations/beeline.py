aef2153ba87b5264bfb83326262960ec5baa327c
yutengchang <yutengchang@gmail.com>
2016-02-26 16:41:39 -0500

    beeline updates

--- configurations/beeline.py
+++ configurations/beeline.py
@@ -11,6 +11,12 @@ format_table = ['--outputformat=table']
 
 
 def bln_prepare_hiveql(formatting):
+    """
+    return the beeline executable with possible overwritten outputput format
+    :param formatting: the parameter used to specify the hive output
+    :return: return string of executable and parameters
+    """
+
     if (formatting == 'csv') | (formatting == 'CSV'):
         return list_hive + format_csv
     elif (formatting == 'csv2') | (formatting == 'CSV2'):
@@ -21,30 +27,56 @@ def bln_prepare_hiveql(formatting):
         return list_hive + format_tsv2
     elif (formatting == 'dsv') | (formatting == 'DSV'):
         return list_hive + format_dsv
+    elif (formatting == 'table') | (formatting == 'TABLE'):
+        return list_hive + format_table
     else:
         return list_hive
 
 
 def bln_e(cmd, outformat='tsv2', database=''):
-    """create corresponding hive -e + command, add "use $database" if specified"""
-    list_used = BL_prepare_hiveql(outformat) + ['-e']
+    """
+    create corresponding hive -e + command, add "use $database" if specified
+
+    :param cmd: hive command
+    :param outformat: output format of hive
+    :param database: hive database
+    :return: no return
+    """
+
+    list_used = bln_prepare_hiveql(outformat) + ['-e']
     if database:
         cmd = 'use %s; ' + cmd
     list_used.append(cmd)
     sp.check_call(list_used)
 
 
-def BL_f(hive_script, outformat='tsv2'):
-    """create corresponding hive -f + hive_script_file"""
-    list_used = BL_prepare_hiveql(outformat) + ['-f']
+def bln_f(hive_script, outformat='tsv2'):
+    """
+    create corresponding hive -f + hive_script_file
+
+    :param hive_script: the hive script being run
+    :param outformat: output format of hive
+    :return: no return
+    """
+
+    list_used = bln_prepare_hiveql(outformat) + ['-f']
     list_used.append(hive_script)
     sp.check_call(list_used)
 
 
-def BL_e_outcall(cmd, outputfile, outformat='tsv2', database=''):
-    """ create corresponding hive -e + command, add "use $database" if
-    specified. take the output from the command. """
-    list_used = BL_prepare_hiveql(outformat) + ['-e']
+def bln_e_outcall(cmd, outputfile, outformat='tsv2', database=''):
+    """
+    create corresponding hive -e + command, add "use $database" if
+    specified. take the output from the command.
+
+    :param cmd: hive command
+    :param outputfile: the output file takes the result
+    :param outformat: output format of hive
+    :param database: hive database
+    :return: no return
+    """
+
+    list_used = bln_prepare_hiveql(outformat) + ['-e']
     if database:
         cmd = 'use %s; ' + cmd
     list_used.append(cmd)
