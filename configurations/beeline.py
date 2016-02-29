b674b22b3aa1e8b1130663c6739baab198fe3475
yutengchang <yutengchang@gmail.com>
2016-02-29 15:12:35 -0500

    update bln with shell=True and add akamai use HADOOP_USER_NAME

--- configurations/beeline.py
+++ configurations/beeline.py
@@ -1,38 +1,40 @@
 import os, sys
 import subprocess as sp
+import shlex
 
 #list_hive = ['bln']
-list_hive = ['/a/third-party/hive/bin//beeline','-u','jdbc:hive2://','-n','""','-p','""','--silent=true']
+string_hive = 'beeline -u jdbc:hive2:// -n "" -p "" --silent=true --outputformat=tsv2 '
+list_hive = ['/a/third-party/hive/bin/beeline','-u','jdbc:hive2://','-n','""','-p','""','--silent=true']
 
-format_tsv2 = ['--outputformat=tsv2']
-format_tsv = ['--outputformat=tsv']
-format_csv2 = ['--outputformat=csv2']
-format_csv = ['--outputformat=csv']
-format_dsv = ['--outputformat=dsv']
-format_table = ['--outputformat=table']
+format_tsv2 = ['--outputformat=tsv2 ']
+format_tsv = ['--outputformat=tsv ']
+format_csv2 = ['--outputformat=csv2 ']
+format_csv = ['--outputformat=csv ']
+format_dsv = ['--outputformat=dsv ']
+format_table = ['--outputformat=table ']
 
 
 def bln_prepare_hiveql(formatting):
     """
-    return the beeline executable with possible overwritten outputput format
+    return the beeline executable with possible overwritten output format
     :param formatting: the parameter used to specify the hive output
     :return: return string of executable and parameters
     """
 
     if (formatting == 'csv') | (formatting == 'CSV'):
-        return list_hive + format_csv
+        return string_hive + format_csv
     elif (formatting == 'csv2') | (formatting == 'CSV2'):
-        return list_hive + format_csv2
+        return string_hive + format_csv2
     elif (formatting == 'tsv') | (formatting == 'TSV'):
-        return list_hive + format_tsv
+        return string_hive + format_tsv
     elif (formatting == 'tsv2') | (formatting == 'TSV2'):
-        return list_hive + format_tsv2
+        return string_hive + format_tsv2
     elif (formatting == 'dsv') | (formatting == 'DSV'):
-        return list_hive + format_dsv
+        return string_hive + format_dsv
     elif (formatting == 'table') | (formatting == 'TABLE'):
-        return list_hive + format_table
+        return string_hive + format_table
     else:
-        return list_hive
+        return string_hive
 
 
 def bln_e(cmd, outformat='tsv2', database=''):
@@ -46,14 +48,12 @@ def bln_e(cmd, outformat='tsv2', database=''):
     :return: no return
     """
 
-    list_used = bln_prepare_hiveql(outformat) + ['-e']
     if database:
         cmd = 'use %s; ' + cmd
-    #cmd = '"'+cmd+'"'
-    list_used.append(cmd)
+    list_used = shlex.split(bln_prepare_hiveql(outformat) + '-e "%s"' % cmd)
     print "this is the list used: "
     print list_used
-    sp.check_call(list_used, shell=True)
+    sp.check_call(list_used)
 
 
 def bln_f(hive_script, outformat='tsv2'):
@@ -65,11 +65,10 @@ def bln_f(hive_script, outformat='tsv2'):
     :return: no return
     """
 
-    list_used = bln_prepare_hiveql(outformat) + ['-f']
-    list_used.append(hive_script)
-    sp.check_call(list_used, shell=True)
+    list_used = shlex.split(bln_prepare_hiveql(outformat) + '-f %s' % hive_script)
+    sp.check_call(list_used)
 
-
+# not used function (not updated yet)
 def bln_e_outcall(cmd, outputfile, outformat='tsv2', database=''):
     """
     create corresponding hive -e + command, add "use $database" if
@@ -87,5 +86,5 @@ def bln_e_outcall(cmd, outputfile, outformat='tsv2', database=''):
         cmd = 'use %s; ' + cmd
     list_used.append(cmd)
     file_handle = open(outputfile, 'w')
-    sp.call(list_used, stdout=file_handle, shell=True)
+    sp.call(list_used, stdout=file_handle)
     file_handle.close()
