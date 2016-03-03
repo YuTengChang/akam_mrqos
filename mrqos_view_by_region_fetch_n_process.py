002055674bc4334163db56c9855b9970bca97ae5
yutengchang <yutengchang@gmail.com>
2016-03-03 15:06:01 -0500

    mrqos_region procedure

--- mrqos_view_by_region_fetch_n_process.py
+++ mrqos_view_by_region_fetch_n_process.py
@@ -18,6 +18,8 @@ def main():
     """
 
     # variable settings
+    query_retry_time = 1
+    max_retrial = 20
     mapmon_machine = "72.246.193.143"
     mapmon_file = "/home/testgrp/full-table-mrqos-view-by-region"
     local_dir = "/home/ychang/Documents/Projects/18-DDC/MRQOS_local_data"
@@ -38,7 +40,19 @@ def main():
     print "    ****  scp file to local"
     sp.check_call(scp_from_mapmon, shell=True)
 
+    # if query failed, re-try:
+    while os.stat(os.path.join(local_dir, 'temp.csv')).st_size < 10000:
+        print "    ****  obtaining from mapmon."
+        cmd_str = """ gwsh -2 %s "%s" """ % ( mapmon_machine, mapmon_command )
+        sp.check_call(cmd_str, shell=True)
+        print "    ****  scp file to local"
+        sp.check_call(scp_from_mapmon, shell=True)
+        query_retry_time += 1
+        if query_retry_time > max_retrial:
+            print "    **** reach max re-trial, quitting..."
+            return
 
+    #
 
 if __name__ == '__main__':
     sys.exit(main())
