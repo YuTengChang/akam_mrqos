b5368214e8175ebf866be79104aac9453cc5e866
yutengchang <yutengchang@gmail.com>
2016-03-09 14:30:58 -0500

    python procedure for hourly summary

---
+++ mrqos_python_script/region_summary_hour.py
@@ -0,0 +1,23 @@
+#!/home/ychang/anaconda/bin/python2.7
+"""
+Created on Thu Apr 16 16:47:15 2015
+
+@author: ychang
+"""
+import sys, os
+import shutil
+
+sys.path.append('/home/testgrp/MRQOS/')
+import subprocess as sp
+import time
+import YT_Timeout as ytt
+import configurations.config as config
+import configurations.hdfsutil as hdfsutil
+import configurations.beeline as beeline
+
+def main():
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
