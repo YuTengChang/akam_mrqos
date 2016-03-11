f6b978864a965ac02abfbbc5f1ce7e94eaf508c3
yutengchang <yutengchang@gmail.com>
2016-03-10 23:09:04 -0500

    testing queries: region view

--- mrqos_hive_query/mrqos_region_view.sql
+++ mrqos_hive_query/mrqos_region_view.sql
@@ -4,9 +4,9 @@ use mrqos;
 select region, collect_set(info) distribution
 from
 (
-    select a.*, b.region_ra_load, round(100*a.case_ra_load/b.region_ra_load,2) case_load_perc, concat(a.casename,"(",round(100*a.case_ra_load/b.region_ra_load,2),")") info from
+    select a.region, round(100*a.case_region_load/b.caseload,2) case_load_perc, concat(a.casename,"(",round(100*a.case_region_load/b.caseload,2),"% of", b.caseload,")") info from
     (
-        select sum(ra_load) case_ra_load,
+        select sum(ra_load) case_region_load,
                concat("MR_",maprule,":GEO_",geoname,":",netname) casename,
                region
         from mrqos_region
@@ -14,9 +14,17 @@ from
     ) a
     inner join
     (
-        select region, sum(ra_load) region_ra_load from mrqos_region group by region
+        select a1.casename, sum(a1.ra_load) caseload from
+        (select *, concat("MR_",maprule,":GEO_",geoname,":",netname) casename from mrqos_region) a1
+        group by a1.casename
     ) b
-    on a.region=b.region
+    on a.casename=b.casename
 ) allbox
 group by region
+limit 3;
+
+
+select a1.casename, sum(a1.ra_load) caseload from
+(select *, concat("MR_",maprule,":GEO_",geoname,":",netname) casename from mrqos_region) a1
+group by a1.casename
 limit 3;
