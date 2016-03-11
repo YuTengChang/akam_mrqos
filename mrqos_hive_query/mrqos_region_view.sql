da359413be3de2d7b670006e44211fe2a90b2e48
yutengchang <yutengchang@gmail.com>
2016-03-10 22:52:36 -0500

    testing queries: region view

---
+++ mrqos_hive_query/mrqos_region_view.sql
@@ -0,0 +1,22 @@
+use mrqos;
+
+
+select region,
+from
+(
+    select a.*, b.region_ra_load, round(100*a.ra_load/b.region_ra_load,2) case_load_perc from
+    (
+        select sum(ra_load) case_ra_load,
+               concat("MR_",maprule,":GEO_",geoname,":",netname) casename,
+               region
+        from mrqos_region
+        group by maprule, geoname, netname, region
+    ) a
+    inner join
+    (
+        select region, sum(ra_load) region_ra_load from mrqos_region group by region
+    ) b
+    on a.region=b.region
+) allbox
+group by region
+limit 3;
