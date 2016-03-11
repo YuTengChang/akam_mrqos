ab2fc56e34f0b94692436cde860e3b11b931a1ef
yutengchang <yutengchang@gmail.com>
2016-03-10 22:59:23 -0500

    testing queries: region view

--- mrqos_hive_query/mrqos_region_view.sql
+++ mrqos_hive_query/mrqos_region_view.sql
@@ -1,10 +1,10 @@
 use mrqos;
 
 
-select region,
+select region, collect_set(info) distribution
 from
 (
-    select a.*, b.region_ra_load, round(100*a.ra_load/b.region_ra_load,2) case_load_perc from
+    select a.*, b.region_ra_load, round(100*a.case_ra_load/b.region_ra_load,2) case_load_perc, concat(a.casename,"(",round(100*a.case_ra_load/b.region_ra_load,2),")") info from
     (
         select sum(ra_load) case_ra_load,
                concat("MR_",maprule,":GEO_",geoname,":",netname) casename,
