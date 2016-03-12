459796920ae527501383434dd5bed3e66e9d3e65
yutengchang <yutengchang@gmail.com>
2016-03-12 09:04:55 -0500

    test ecor view

--- mrqos_hive_query/mrqos_region_view.sql
+++ mrqos_hive_query/mrqos_region_view.sql
@@ -64,36 +64,41 @@ group by a1.casename
 limit 3;
 
 
+
 (
-    SELECT
-        a.region, a.ecor, a.continent, a.country, a.city, a.latitude, a.longitude, a.asnum, a.provider, a.ecor_capacity,
-        a.prp, a.numghosts, b.case_region_load, b.casename
-    FROM
-    (
+SELECT ecor, collect_set(continent)[0] continent, collect_set(country)[0] country, collect_set(city)[0] city,
+    collect_set(round(latitude,3))[0] latitude, collect_set(round(longitude,3))[0] longitude, collect_set(asnum)[0] asnum,
+    collect_set(provider) provider, collect_set(ecor_capacity) ecor_cap, collect_set(prp)[0] prp, sum(numghosts) numghosts
+FROM
+( SELECT * from mapper.barebones a1, (select max(day) maxday from mapper.barebones) a2 where a1.day=a2.maxday) a3
+GROUP BY ecor
+) ecor_info
+
+
+
         SELECT
-            a1.region,
-            a1.ecor,
-            a1.continent,
-            a1.country,
-            a1.city,
-            round(a1.latitude,3) latitude,
-            round(a1.longitude,3) longitude,
-            a1.asnum,
-            a1.provider,
-            a1.ecor_capacity,
-            a1.prp,
-            a1.numghosts
-        FROM mapper.barebones a1, (select max(day) maxday from mapper.barebones) a2 where a1.day=a2.maxday
-    ) a
-    INNER JOIN
-    (
-        SELECT sum(ra_load) case_region_load,
-               concat("MR_",maprule,":GEO_",geoname,":",netname) casename,
-               region
-        FROM mrqos_region
-        WHERE datestamp=20160311 AND hour=05
-        GROUP BY maprule, geoname, netname, region
-    ) b
-    ON a.region=b.region
-) c
-GROUP BY ecor,
+            a.region, c.ecor, a.case_region_load, a.casename, b.caseload
+        FROM
+        (
+            SELECT sum(ra_load) case_region_load,
+                   concat("MR_",maprule,":GEO_",geoname,":",netname) casename,
+                   region
+            FROM mrqos_region
+            WHERE datestamp=20160311 AND hour=05
+            GROUP BY maprule, geoname, netname, region
+        ) a
+        INNER JOIN
+        (
+            SELECT a1.casename, sum(a1.ra_load) caseload FROM
+            (
+                SELECT *, concat("MR_",maprule,":GEO_",geoname,":",netname) casename
+                FROM mrqos_region
+                WHERE datestamp=20160311 AND hour=05
+            ) a1
+            GROUP BY a1.casename
+        ) b
+        ON a.casename=b.casename
+        INNER JOIN
+        (SELECT region, ecor from mapper.barebones a1, (select max(day) maxday from mapper.barebones) a2 where a1.day=a2.maxday) c
+        ON a.region=c.region
+        LIMIT 2;
