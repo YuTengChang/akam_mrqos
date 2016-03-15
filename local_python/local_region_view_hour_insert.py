f0cdfcd970d7bce56dc6df7d1f9e29122fbd3298
yutengchang <yutengchang@gmail.com>
2016-03-15 11:31:12 -0400

    data insert to the sqlite3 database

--- local_python/local_region_view_hour_insert.py
+++ local_python/local_region_view_hour_insert.py
@@ -5,7 +5,7 @@ Created on Thu March 09 12:47:15 2016
 @author: ychang
 """
 import sys, os
-#import shutil
+import shutil
 import time
 import calendar
 import subprocess as sp
@@ -22,10 +22,11 @@ def main():
     filename = 'region_view_hour.%s.%s.csv' % (datestamp, hourstamp)
     local_data_repot = '/home/ychang/Documents/Projects/18-DDC/MRQOS_local_data/region_view_hour/'
     local_file = os.path.join(local_data_repot, filename)
+    local_temp = os.path.join(local_data_repot, 'temp.csv')
 
     # fetch the data file from the cluster
     try:
-        cmd_str = 'scp -Sgwsh testgrp@81.52.137.195:/home/testgrp/query_results/%s %s' % (filename, local_file)
+        cmd_str = 'scp -Sgwsh testgrp@81.52.137.195:/home/testgrp/query_results/%s %s' % (filename, local_temp)
         sp.check_call(cmd_str, shell=True)
         # cluster file remove
         cmd_str = 'gwsh 81.52.137.195 "rm /home/testgrp/query_results/%s"' % filename
@@ -35,10 +36,19 @@ def main():
         return
 
     # insert into the SQLite3 database
-    cmd_str = '/opt/anaconda/bin/sqlite3 /opt/web-data/SQLite3/ra_mrqos.db ".import %s region_view_hour"' % local_file
+    cmd_str = "cat %s | tail -n+2 | sed 's/\t/,/g' > %s" % (local_temp, local_file)
+    sp.check_call(cmd_str, shell=True)
+
+    cmd_str = "echo '.separator ,' > %s" % os.path.join(local_data_repot, 'input_query.sql')
+    sp.check_call(cmd_str, shell=True)
+    cmd_str = "echo '.import %s region_view_hour' >> %s" % (local_file, os.path.join(local_data_repot, 'input_query.sql'))
+    sp.check_call(cmd_str, shell=True)
+
+    cmd_str = '/opt/anaconda/bin/sqlite3 /opt/web-data/SQLite3/ra_mrqos.db < %s' % os.path.join(local_data_repot, 'input_query.sql')
     sp.check_call(cmd_str, shell=True)
 
     # remove local file
+    os.remove(local_temp)
     os.remove(local_file)
 
     # expire the data from SQLite database
