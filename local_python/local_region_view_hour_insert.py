4aa788ff3d668d060320838ac13ef4a44ea245c4
yutengchang <yutengchang@gmail.com>
2016-03-15 10:20:02 -0400

    manage local python insert of region_view_hour data

---
+++ local_python/local_region_view_hour_insert.py
@@ -0,0 +1,23 @@
+#!/opt/anaconda/bin/python
+"""
+Created on Thu March 09 12:47:15 2016
+
+@author: ychang
+"""
+import sys, os
+#import shutil
+import time
+import calendar
+import subprocess as sp
+
+def main():
+    ts = calendar.timegm(time.gmtime())
+    print "###################"
+    print "# Performing the hourly mrqos_region summary"
+    print "# starting processing time is " + str(ts)
+    print "###################"
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
