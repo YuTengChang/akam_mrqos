a29174cddddaea71c9847339e504dcaa2e1bfe96
yutengchang <yutengchang@gmail.com>
2016-03-15 22:04:33 -0400

    add back the region_view_hour processing

--- mrqos_python_script/region_summary_hour.py
+++ mrqos_python_script/region_summary_hour.py
@@ -81,56 +81,56 @@ def main():
     # ############################## #
     # The REGION VIEW hive procedure #
     # ############################## #
-#    print "    ****  region view tour:"
-#    # check if the summary has been performed on this particular hour (last hour)
-#    print "    ****  checking day = %s, hour = %s." % (datestamp, hourstamp),
-#    if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), '000000_0.deflate')):
-#        f = open(os.path.join(config.mrqos_hive_query, 'mrqos_region_view_hour.hive'), 'r')
-#        strcmd = f.read()
-#        strcmd_s = strcmd % (datestamp, hourstamp, datestamp, hourstamp, datestamp, hourstamp)
-#        f.close()
-#        strcmd_g = "select * from mrqos.region_view_hour where datestamp=%s and hour=%s;" % (datestamp, hourstamp)
-#        query_result_file = os.path.join(config.mrqos_query_result,'region_view_hour.%s.%s.csv' % (datestamp, hourstamp))
-#        print "    ****  perform beeline for hourly summary for day = %s, hour = %s." % (datestamp, hourstamp),
-#        count_retrial = 0
-#        while count_retrial < region_summary_retrial_max:
-#            try:
-#                tic = time.time()
-#                beeline.bln_e(strcmd_s)
-#                print "time cost = %s." % str(time.time()-tic)
-#                try:
-#                    beeline.bln_e_output(strcmd_g, query_result_file)
-#                except:
-#                    print "    ****  copy to local failed, retry!"
-#                    beeline.bln_e_output(strcmd_g, query_result_file)
-#                break
-#            except:
-#                # delete the folder if summarization failed.
-#                print "    ****  summarization failed with time cost %s upto #retrials=%s" % (str(time.time()-tic), str(count_retrial))
-#                hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), r=True)
-#                count_retrial += 1
+    print "    ****  region view tour:"
+    # check if the summary has been performed on this particular hour (last hour)
+    print "    ****  checking day = %s, hour = %s." % (datestamp, hourstamp),
+    if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), '000000_0.deflate')):
+        f = open(os.path.join(config.mrqos_hive_query, 'mrqos_region_view_hour.hive'), 'r')
+        strcmd = f.read()
+        strcmd_s = strcmd % (datestamp, hourstamp, datestamp, hourstamp, datestamp, hourstamp)
+        f.close()
+        strcmd_g = "select * from mrqos.region_view_hour where datestamp=%s and hour=%s;" % (datestamp, hourstamp)
+        query_result_file = os.path.join(config.mrqos_query_result,'region_view_hour.%s.%s.csv' % (datestamp, hourstamp))
+        print "    ****  perform beeline for hourly summary for day = %s, hour = %s." % (datestamp, hourstamp),
+        count_retrial = 0
+        while count_retrial < region_summary_retrial_max:
+            try:
+                tic = time.time()
+                beeline.bln_e(strcmd_s)
+                print "time cost = %s." % str(time.time()-tic)
+                try:
+                    beeline.bln_e_output(strcmd_g, query_result_file)
+                except:
+                    print "    ****  copy to local failed, retry!"
+                    beeline.bln_e_output(strcmd_g, query_result_file)
+                break
+            except:
+                # delete the folder if summarization failed.
+                print "    ****  summarization failed with time cost %s upto #retrials=%s" % (str(time.time()-tic), str(count_retrial))
+                hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), r=True)
+                count_retrial += 1
 
-#    else:
-#        print " file exists."
+    else:
+        print " file exists."
 
     # check if the summary has been performed since the beginning of the day, last check on day X is X+1/0:30:00
-#    for hour in hour_list:
-#        if hour < hourstamp:
-#            print "    ****  checking day = %s, hour = %s." % (datestamp, hour),
-#            if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hour), '000000_0.deflate')):
-#                f = open(os.path.join(config.mrqos_hive_query, 'mrqos_region_view_hour.hive'), 'r')
-#                strcmd = f.read()
-#                strcmd_s = strcmd % (datestamp, hour, datestamp, hour, datestamp, hour)
-#                f.close()
-#                print "    ****  perform beeline for hourly summary for day = %s, hour = %s." %(datestamp, hour)
-#                try:
-#                    beeline.bln_e(strcmd_s)
-#                except:
-#                    # delete the folder if summarization failed.
-#                    print "    ****  summarization failed, removed hdfs folder."
-#                    hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hour), r=True)
-#            else:
-#                print " file exists."
+    for hour in hour_list:
+        if hour < hourstamp:
+            print "    ****  checking day = %s, hour = %s." % (datestamp, hour),
+            if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hour), '000000_0.deflate')):
+                f = open(os.path.join(config.mrqos_hive_query, 'mrqos_region_view_hour.hive'), 'r')
+                strcmd = f.read()
+                strcmd_s = strcmd % (datestamp, hour, datestamp, hour, datestamp, hour)
+                f.close()
+                print "    ****  perform beeline for hourly summary for day = %s, hour = %s." %(datestamp, hour)
+                try:
+                    beeline.bln_e(strcmd_s)
+                except:
+                    # delete the folder if summarization failed.
+                    print "    ****  summarization failed, removed hdfs folder."
+                    hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hour), r=True)
+            else:
+                print " file exists."
 
 
 
