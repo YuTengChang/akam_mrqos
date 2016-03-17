128a4cd5edfb687ec03c9a4fe54b2b55b908888b
yutengchang <yutengchang@gmail.com>
2016-03-17 10:05:10 -0400

    debug table for case_view_hour process

--- mrqos_python_script/region_summary_hour.py
+++ mrqos_python_script/region_summary_hour.py
@@ -24,7 +24,7 @@ def main():
     ts_last_hour = ts-3600
     datestamp = time.strftime('%Y%m%d', time.gmtime(float(ts_last_hour)))
     hourstamp = time.strftime('%H', time.gmtime(float(ts_last_hour)))
-    hour_list = [str("%02d" % x) for x in range(24)]
+    #hour_list = [str("%02d" % x) for x in range(24)]
     region_summary_retrial_max = 10
 
     # ############################### #
@@ -86,10 +86,10 @@ def main():
     #            print " file exists."
 
 
-    # ############################## #
-    # The REGION VIEW hive procedure #
-    # ############################## #
-    print "    ****  region view tour:"
+    # ############################ #
+    # The CASE VIEW hive procedure #
+    # ############################ #
+    print "    ****  case view tour:"
     # check if the summary has been performed on this particular hour (last hour)
     print "    ****  checking day = %s, hour = %s." % (datestamp, hourstamp),
     if hdfsutil.test_file(os.path.join(config.hdfs_qos_case_view_hour % (datestamp, hourstamp), '000000_0.deflate')):
@@ -116,39 +116,18 @@ def main():
             except:
                 # delete the folder if summarization failed.
                 print "    ******  failed with time cost = %s upto #retrials=%s" % (str(time.time()-tic), str(count_retrial))
-                hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), r=True)
+                hdfsutil.rm(config.hdfs_qos_case_view_hour % (datestamp, hourstamp), r=True)
                 count_retrial += 1
 
     else:
         print " file exists."
 
-    # check if the summary has been performed since the beginning of the day, last check on day X is X+1/0:30:00
-    #for hour in hour_list:
-    #    if hour < hourstamp:
-    #        print "    ****  checking day = %s, hour = %s." % (datestamp, hour),
-    #        if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hour), '000000_0.deflate')):
-    #            print " file not exits,",
-    #            f = open(os.path.join(config.mrqos_hive_query, 'mrqos_region_view_hour.hive'), 'r')
-    #            strcmd = f.read()
-    #            strcmd_s = strcmd % (datestamp, hour, datestamp, hour, datestamp, hour)
-    #            f.close()
-    #            print " BLN for hourly summary for day = %s, hour = %s." %(datestamp, hour)
-    #            try:
-    #                tic = time.time()
-    #                beeline.bln_e(strcmd_s)
-    #                print "    ******  success with time cost = %s." % str(time.time()-tic)
-    #            except:
-    #                # delete the folder if summarization failed.
-    #                print "    ******  failed with time cost = %s, removed hdfs folder." % (str(time.time()-tic))
-    #                # hdfsutil.rm(config.hdfs_qos_rg_view_hour % (datestamp, hour), r=True)
-    #        else:
-    #            print " file exists."
 
 
-    # ############################ #
-    # The CASE VIEW hive procedure #
-    # ############################ #
-    print "    ****  case view tour:"
+    # ############################## #
+    # The REGION VIEW hive procedure #
+    # ############################## #
+    print "    ****  region view tour:"
     # check if the summary has been performed on this particular hour (last hour)
     print "    ****  checking day = %s, hour = %s." % (datestamp, hourstamp),
     if hdfsutil.test_file(os.path.join(config.hdfs_qos_rg_view_hour % (datestamp, hourstamp), '000000_0.deflate')):
