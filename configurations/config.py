e27bcc8bd88f3c035f13d897d4cffc962401300a
yutengchang <yutengchang@gmail.com>
2016-03-26 21:42:10 -0400

    add scidb server location

--- configurations/config.py
+++ configurations/config.py
@@ -75,6 +75,7 @@ add_rg_partition = 'use MRQOS; alter table mrqos_region add partition(datestamp=
 #==============================================================================
 region_view_hour_data_source = '81.52.137.180'
 web_server_machine = '172.25.23.222' #(need to check periodically against: 'dev-platformperf-scidb02.kendall.corp.akamai.com')
+
 region_view_hour_db = '/opt/web-data/SQLite3/ra_mrqos.db'
 region_view_hour_init = '/u4/ychang/Projects/18-MRQOS/SQLite3/region_view_hour_init.sql'
 case_view_hour_db = '/opt/web-data/SQLite3/case_view_hour.db'
