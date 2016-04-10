5684a552ebe9fae734dbe00197e7853326410a73
yutengchang <yutengchang@gmail.com>
2016-04-09 21:18:03 -0400

    move the clean up of sqlite db to vm separate cron jobs

--- local_python/vm_daily_sqlite_cleanup.py
+++ local_python/vm_daily_sqlite_cleanup.py
@@ -16,25 +16,28 @@ def main():
     ts = calendar.timegm(time.gmtime())
     region_view_hour_db = '/opt/web-data/SQLite3/ra_mrqos.db'
     case_view_hour_db = '/opt/web-data/SQLite3/case_view_hour.db'
-
+    region_view_hour_db_2 = '/opt/web-data/SQLite3/ra_mrqos_b2.db'
+    case_view_hour_db_2 = '/opt/web-data/SQLite3/case_view_hour_b2.db'
 
     print "now do the cleaning on VM: region_view_hour"
     expire_region_view_hour_vm = 60*60*24*(4+1) # 1+4 days expiration (~ 1-week)
     expire_date = time.strftime('%Y%m%d', time.gmtime(float(ts - expire_region_view_hour_vm)))
+    shutil.copyfile(region_view_hour_db, region_view_hour_db_2)
     sql_str = '''PRAGMA temp_store_directory='/opt/web-data/temp'; delete from region_view_hour where date=%s; vacuum;''' % str(expire_date)
-    cmd_str = '''/opt/anaconda/bin/sqlite3 %s "%s"  ''' % (region_view_hour_db,
+    cmd_str = '''/opt/anaconda/bin/sqlite3 %s "%s"  ''' % (region_view_hour_db_2,
                                                            sql_str)
-    # print cmd_str
     sp.check_call(cmd_str, shell=True)
+    shutil.move(region_view_hour_db_2, region_view_hour_db)
 
     print "now do the cleaning on VM: case_view_hour"
     expire_region_view_hour_vm = 60*60*24*(4+1) # 1+4 days expiration (~ 1-week)
     expire_date = time.strftime('%Y%m%d', time.gmtime(float(ts - expire_region_view_hour_vm)))
+    shutil.copyfile(case_view_hour_db, case_view_hour_db_2)
     sql_str = '''PRAGMA temp_store_directory='/opt/web-data/temp'; delete from case_view_hour where date=%s; vacuum;''' % str(expire_date)
-    cmd_str = '''/opt/anaconda/bin/sqlite3 %s "%s"  ''' % (case_view_hour_db,
+    cmd_str = '''/opt/anaconda/bin/sqlite3 %s "%s"  ''' % (case_view_hour_db_2,
                                                            sql_str)
-    # print cmd_str
     sp.check_call(cmd_str, shell=True)
+    shutil.move(case_view_hour_db_2, case_view_hour_db)
 
 if __name__ == '__main__':
     sys.exit(main())
