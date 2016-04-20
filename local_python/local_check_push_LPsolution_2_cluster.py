bd238026b81d7f1a25c42d3e4fd3bb15725f6604
yutengchang <yutengchang@gmail.com>
2016-04-20 16:00:26 -0400

    local check and push LP solution to the cluster procedure

--- local_python/local_check_push_LPsolution_2_cluster.py
+++ local_python/local_check_push_LPsolution_2_cluster.py
@@ -20,10 +20,17 @@ def main():
     print "###################"
     print "  >> check the LP solutions <<"
 
+    local_lp_folder = '/home/ychang/Documents/Projects/18-DDC/MRQOS_local_data/lp'
+
     cmd_str = """ssh %s -A 'ls -d /opt/lp/*/' """ % config.web_server_machine
     filelist = sp.check_output(cmd_str, shell=True)
     production_filelist = [y for y in [x.split('/')[-2] for x in filelist.split('\n')[:-1]] if y[-5:] == '_prod']
     print production_filelist
+
+    for file in production_list:
+        cmd_str = 'scp ychang@dev-platformperf-scidb02:/opt/lp/%s %s' % (file, os.path.join(local_lp_folder, file))
+        sp.check_call(cmd_str, shell=True)
+
 
 if __name__ == '__main__':
     sys.exit(main())
