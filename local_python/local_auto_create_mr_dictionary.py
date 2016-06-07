c7465c540ec94a02bedeac8791e4c83e6ea0dca5
yutengchang <yutengchang@gmail.com>
2016-06-07 17:05:29 -0400

    automatic query for the mr_auto_dictionary.txt

--- local_python/local_auto_create_mr_dictionary.py
+++ local_python/local_auto_create_mr_dictionary.py
@@ -15,9 +15,11 @@ import configurations.config as config
 def main():
     table_local_dir = '/u4/ychang/Projects/18-MRQOS/Data/'
     table_local_file = 'mr_auto_dictionary.txt'
+    formatting = ' --format=left --noprompt '
     sql_cmd = ''' select mapruleid, service, description, mrname + ' [' + cast(mapruleid as string) + ']' mrn, priority from (select mapruleid, substr(name, 3, instr(name,'.')-3) mrname, case service when 'W' then 'FreeFlow' when 'S' then 'ESSL' else 'Other' end service,  priority, description from mcm_maprules limit 5); '''
-    sp_cmd = '''/usr/bin/sql2 -qmap.dev.query.akadns.net "%s" | tail -n+3 | head -n-2 > %s ''' % ( sql_cmd,
-                                                                                                   os.path.join(table_local_dir, table_local_file) )
+    sp_cmd = '''/usr/bin/sql2 -qmap.dev.query.akadns.net %s "%s" | tail -n+3 | head -n-2 > %s ''' % ( formatting,
+                                                                                                      sql_cmd,
+                                                                                                      os.path.join(table_local_dir, table_local_file) )
     sp.check_call(sp_cmd, shell=True)
 
 if __name__ == '__main__':
