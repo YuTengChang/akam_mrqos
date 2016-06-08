da88caf942a5dee364a930052934b177b52ff42e
yutengchang <yutengchang@gmail.com>
2016-06-08 11:29:00 -0400

    automatic query for the mr_auto_dictionary.txt

--- local_python/local_auto_create_mr_dictionary.py
+++ local_python/local_auto_create_mr_dictionary.py
@@ -8,6 +8,8 @@ import sys, os
 import shutil
 import time
 import calendar
+import pandas as pd
+import numpy
 import subprocess as sp
 sys.path.append('/home/ychang/Documents/Projects/18-DDC/MRQOS/')
 import configurations.config as config
@@ -21,6 +23,26 @@ def main():
                                                                                           sql_cmd,
                                                                                           os.path.join(table_local_dir, table_local_file) )
     sp.check_call(sp_cmd, shell=True)
+
+    # sync with old file (mr_dictionary)
+    filename = 'mr_dictionary.txt'
+    file_source =  os.path.join(table_local_dir, filename)
+    data = numpy.genfromtxt(file_source, delimiter=',', skip_header=0, dtype='str')
+    headers = ["maprule1", "service1", "description1", "mname1", "priority1"]
+    df = pd.DataFrame(data, columns=headers)
+    file_source =  os.path.join(table_local_dir, table_local_file)
+    data = numpy.genfromtxt(file_source, delimiter=',', skip_header=0, dtype='str')
+    headers = ["maprule", "service", "description", "mname", "priority"]
+    df2 = pd.DataFrame(data, columns=headers)
+
+    df.index = df.maprule1
+    df2.index = df2.maprule
+    df_all = pd.concat([df2,df], axis=1, join_axes=[df2.index])
+    df_all.fillna(0, inplace=True)
+    df_all['description'] = [x if y==0 else y for (x,y) in zip(df_all.description, df_all.description1)]
+    df_all.drop(['maprule1', 'service1', 'description1', 'mname1', 'priority1'], axis=1, inplace=True)
+
+    df_all.to_csv(os.path.join(table_local_dir, 'mr_final_dictionary.txt'), header=False, index=False)
 
 if __name__ == '__main__':
     sys.exit(main())
