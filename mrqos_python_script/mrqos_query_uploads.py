8d7a6125966dd9c6569984fefd4e3e73f2e55b81
yutengchang <yutengchang@gmail.com>
2016-08-01 15:07:39 -0400

    alter the agg for in-out-ratio query.

--- mrqos_python_script/mrqos_query_uploads.py
+++ mrqos_python_script/mrqos_query_uploads.py
@@ -61,7 +61,47 @@ def main():
             logger.error('error message: %s', e.message)
 
 
-    # TODO: clean up the joined
+    # DONE: clean up the joined
+    mrqos_join_cleanup()
+
+# ==============================================================================
+# # remove partitions from hive table
+# ==============================================================================
+
+def mrqos_join_cleanup():
+    """ when called, this function will delete all partitions
+        the clnspp table as long as it is older than the threshold """
+
+    # get the lowest partition by checking the HDFS folders
+    joined_partitions = hdfsutil.ls(config.hdfs_table_join)
+    str_parts_list = [i.split('=', 1)[1] for i in joined_partitions]
+    str_parts_list_int = map(int, str_parts_list)
+
+    # check if "partitions" is within the threshold
+    timenow = int(time.time())
+
+    # get the list of retired data in HDFS using hive partitions
+    try:
+        hdfs_remove_list = [x for x in beeline.show_partitions('mrqos.mrqos_join').split('\n')\
+                            if '=' in x and x.split('=')[1] < str(timenow-config.mrqos_join_delete)]
+        try:
+            # drop the partitions in hive
+            beeline.drop_partitions('mrqos.mrqos_join', 'ts<%s' % str(timenow-config.mrqos_join_delete))
+            print " drop partitions successful. "
+            # remove the hdfs folders
+            for partition_id in hdfs_remove_list:
+                try:
+                    hdfs_d = os.path.join(config.hdfs_table, 'mrqos_join', '%s' % str(partition_id))
+                    hdfsutil.rm(hdfs_d, r=True)
+                except sp.CalledProcessError as e:
+                    print ">> failed to remove HDFS folder for mrqos_join at partition folder %s" % str(partition_id)
+            print " remove HDFS successful. "
+        except sp.CalledProcessError as e:
+            print ">> failed to drop partitions"
+    except sp.CalledProcessError as e:
+        print ">> failed to obtain retire partition list (HIVE)"
+        print e.message
+
 
 if __name__ == '__main__':
     sys.exit(main())
