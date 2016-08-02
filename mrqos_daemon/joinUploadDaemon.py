ecde60908728a2a885c61e1674225a023403549d
yutengchang <yutengchang@gmail.com>
2016-08-02 14:31:45 -0400

    set up crondaemon for mrqos_join_upload procedure

--- mrqos_daemon/joinUploadDaemon.py
+++ mrqos_daemon/joinUploadDaemon.py
@@ -1,38 +1,20 @@
 import sys
-sys.path.append('..')
+sys.path.append('/home/testgrp/MRQOS/crondaemon')
 import cronDaemon
 import subprocess as sp
 
 
 
-class exampleDaemon(cronDaemon.cronDaemon):
-
-
-    def __init__(self,args):
-        cronDaemon.cronDaemon.__init__(self,args)
-        self.log.info("starting Example")
-
-
-
-    def run_job(self):
-
-        res = sp.check_output("gwsh s187m.ddc.akamai.com '/a/third-party/hadoop/bin/hadoop fs -ls %s'"%args['ls_d'],
-                        shell=True)
-
-        print res
-
-
-
 class joinUploadDaemon(cronDaemon.cronDaemon):
-
-
+    """
+    the child class of the daemon class
+    """
     def __init__(self,args):
         cronDaemon.cronDaemon.__init__(self,args)
         self.log.info("CronDaemon for upload mrqos.mrqos_join/mrqos_join2 data")
 
 
     def run_job(self):
-
         cmdstr = '''/usr/bin/python /home/testgrp/MRQOS/mrqos_python_script/mrqos_query_uploads.py '''
         sp.check_call(cmdstr, shell=True)
 
---
+++ mrqos_daemon/run_mrqos_join_upload_daemon.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('/home/testgrp/MRQOS/crondaemon')
+import joinUploadDaemon as ed
+import backgroundStart as bs
+
+
+if __name__=='__main__':
+
+    """
+    set the arguments:
+    log_file: file location of the .log file
+    log: python object for logger
+    sleep_interval: interval of sequential check, in seconds
+    """
+
+    args = { 'log_file': '/home/testgrp/logs/mrqos_query_uploads.log',
+             'log': 'MRQOS_Join_Upload_Log',
+             'sleep_interval': 10*60}
+
+    dmn = ed.exampleDaemon(args)
+    proc = bs.backgroundStarter(args,'/home/testgrp/pid/mrqos_query_uploads_pid.st',dmn)
+
+    proc.run()
+
