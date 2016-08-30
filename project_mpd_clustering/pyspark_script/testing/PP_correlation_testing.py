023b452e845cd60e0e767ff3eed96538f598c154
yutengchang <yutengchang@gmail.com>
2016-08-30 10:48:17 -0400

    create ppinfo hive table initialization

---
+++ project_mpd_clustering/pyspark_script/testing/PP_correlation_testing.py
@@ -0,0 +1,30 @@
+import sys, os
+
+sys.path.append('/home/testgrp/MRQOS/')
+import subprocess as sp
+import time
+import YT_Timeout as ytt
+import configurations.config as config
+import configurations.hdfsutil as hdfsutil
+import configurations.beeline as beeline
+import logging
+from pyspark import SparkContext
+from pyspark.sql import HiveContext
+
+import math
+import pandas as pd
+import numpy as np
+
+
+def geodesic_distance(lat1, lon1, lat2, lon2):
+    R = 3963.1676
+    lat1r = math.radians(lat1)
+    lat2r = math.radians(lat2)
+    lon1r = math.radians(lon1)
+    lon2r = math.radians(lon2)
+    dlat = abs(lat2r - lat1r)/2
+    dlon = abs(lon2r - lon1r)/2
+    a = math.pow(math.sin(dlat), 2) + math.cos(lat2r) * math.cos(lat1r) * math.pow(math.sin(dlon), 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return [lat1, lon1, lat2, lon2, R*c]
+
