9a73ae9f4a4e97d415c4067e023c7c66ff2e2522
yutengchang <yutengchang@gmail.com>
2016-12-28 13:31:12 -0500

    add procedure to update as-list in sqlite3 db

---
+++ local_python/cron_update_aslist_desktop_and_vm.py
@@ -0,0 +1,42 @@
+#!/opt/anaconda/bin/python
+"""
+Created on Thu March 09 12:47:15 2016
+
+@author: ychang
+"""
+import sys, os
+import subprocess as sp
+sys.path.append('/home/ychang/Documents/Projects/18-DDC/MRQOS/')
+import configurations.config as config
+import logging
+import time
+
+def main():
+    # set up the logger
+    logging.basicConfig(filename=os.path.join(config.local_logging, 'aslist_update.log'),
+                            level=logging.INFO,
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            datefmt='%m/%d/%Y %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    timestamp = time.strftime('%Y%m%d %H:%M:%S', time.gmtime())
+    logger.info('start cron-tasking the aslist updates at date: %s' % (timestamp))
+
+
+    cmd_str = 'python %s' % (os.path.join('/home/ychang/Documents/Projects/18-DDC/MRQOS/','local_python','mrqos_aslist_update.py'))
+    sp.check_call(cmd_str, shell=True)
+
+    if os.path.isfile(config.aslist_file) and os.stat(config.aslist_file).st_size > 0:
+        try:
+            cmd_str = 'scp %s ychang@dev-platformperf-scidb02:/u0/ychang/%s' % (config.aslist_file,
+                                                                                config.aslist_file.split('/')[-1])
+            sp.check_call(cmd_str, shell=True)
+        except:
+            logger.error('error when uploading to scidb02 VM')
+    else:
+        logger.error('updating by query ASlist failed')
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
